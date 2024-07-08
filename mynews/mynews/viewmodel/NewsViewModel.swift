1f84c97e953b19068c10f835956789a7b46e5377
Rubén <ruben.sr.apple@gmail.com>
2024-07-08 12:34:47 +0200

    work in view model api request

--- mynews/mynews/viewmodel/NewsViewModel.swift
+++ mynews/mynews/viewmodel/NewsViewModel.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class NewsViewModel {
+    
+    @Published var articles: [ArticleModel] = []
+    private var cancellables = Set<AnyCancellable>()
+    private let apiKey = "" // TODO add API KEY
+    
+    init() {
+        fetchNews()
+    }
+
+    func fetchNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY")! // TODO add Query
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: NewsResponseModel.self, decoder: JSONDecoder())
+            .map { $0.articles }
+            .replaceError(with: [])
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.articles, on: self)
+            .store(in: &cancellables)
+    }
     
 }
