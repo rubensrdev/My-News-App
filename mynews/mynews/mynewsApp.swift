1f11d77615db33d50f104c76aa931f6b6a29d67c
Rubén <ruben.sr.apple@gmail.com>
2024-07-08 12:21:29 +0200

    Create Xcode project v2

---
+++ mynews/mynews/mynewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mynewsApp.swift
+//  mynews
+//
+//  Created by Ruben on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct mynewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
---
+++ mynews/mynews/view/Home.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mynews
+//
+//  Created by Ruben on 8/7/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Home()
+}
